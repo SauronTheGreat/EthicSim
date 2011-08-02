1e4ef9f3a7ecda2c60cfe085241e1208499c362b
Rushabh Hathi <rushabhhathi@gmail.com>
2011-08-02 12:02:48 +0530

    admin flag added

--- db/migrate/20110802061522_defaultuser.rb
+++ db/migrate/20110802061522_defaultuser.rb
@@ -1,7 +1,7 @@
 class Defaultuser < ActiveRecord::Migration
   def self.up
 
-	User.create(:first_name=>"Admin",:last_name=>"Admin",:email=>"admin@ptotem.com",:username=>"admin",:password=>"p20o20e13",:password_confirmation=>"p20o20e13",:facilitator_group_id=>1)
+	User.create(:first_name=>"Admin",:last_name=>"Admin",:email=>"admin@ptotem.com",:username=>"admin",:password=>"p20o20e13",:password_confirmation=>"p20o20e13",:facilitator_group_id=>1,:admin=>true)
   end
 
   def self.down
