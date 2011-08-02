6243aec85cca8fa05dbed3e3c510e74fc356ecec
Rushabh Hathi <rushabhhathi@gmail.com>
2011-08-02 16:23:21 +0530

    display_controller

---
+++ app/controllers/display_messaging_controller.rb
@@ -0,0 +1,24 @@
+class DisplayMessagingController < ApplicationController
+  def show_modes
+
+  end
+
+
+  def show_messages
+
+	@student_group=StudentGroup.find_by_facilitator_group_id(current_user.facilitator_group_id)
+	if (params[:questionnaire]=="Quiz")
+	  @message=FacilitatorInput.find(:first, :conditions => ['student_group_id = ? and location = ?', @student_group.id, "instr_quiz"])
+	elsif params[:questionnaire]=="PostQuestionnaire"
+	  @message=FacilitatorInput.find(:first, :conditions => ['student_group_id = ? and location = ?', @student_group.id, "instr_post_quiz"])
+	elsif params[:questionnaire]=="Closing"
+		  @message=FacilitatorInput.find(:first, :conditions => ['student_group_id = ? and location = ?', @student_group.id, "instr_closing"])
+		  end
+
+	  if (!@message.nil?)
+		@message=@message.body
+	  end
+
+	end
+  end
+
